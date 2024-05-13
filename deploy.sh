51707c8850bd46065776415e8d216016f61c2299
Vladislav <flickervladislav@gmail.com>
2024-05-13 20:41:16 +0300

    fix: change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git checkout -B main
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:vflicker/map.git main:gh-pages
+git push -f git@github.com:vflicker/image-editor.git main:gh-pages
 
 cd -
